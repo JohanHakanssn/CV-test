73a2aabb758fa961903f109309869f86c3cbb4f6
[Johan Håkansson] <[johan1hakansson@gmail.com]>
2024-02-01 09:38:34 +0100

    .-.

--- src/components/CardSkillsProps.jsx
+++ src/components/CardSkillsProps.jsx
@@ -1,29 +1,21 @@
 import styles from "./CardSkills.module.css";
-import image1 from "../images/CSS-Logo.jpg";
-import image2 from "../images/html-logo.jng";
-import image3 from "../images/Javascript logo.png";
-import image4 from "../images/React-icon.svg.png";
 
 function Skills(props) {
   return (
     <section className={styles.card__container}>
-      <div className={styles.card}>
+      <div className={styles.card} style={{ backgroundColor: "red" }}>
         {props.skill1}
-        <img src={image2} alt="HTML.logo" />
       </div>
 
-      <div className={styles.card}>
+      <div className={styles.card} style={{ backgroundColor: "blue" }}>
         {props.skill2}
-        <img src={image1} alt="CSS.logo" />
       </div>
 
-      <div className={styles.card}>
+      <div className={styles.card} style={{ backgroundColor: "orange" }}>
         {props.skill3}
-        <img src={image3} alt="Javascript.logo" />
       </div>
-      <div className={styles.card}>
+      <div className={styles.card} style={{ backgroundColor: "lightblue" }}>
         {props.skill4}
-        <img src={image4} alt="REACT.logo" />
       </div>
     </section>
   );
