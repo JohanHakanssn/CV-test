db7b1bdcadfd98c77dcc7b6df307026ad73e664c
[Johan Håkansson] <[johan1hakansson@gmail.com]>
2024-02-01 13:49:27 +0100

    test

--- src/pages/Aboutme.jsx
+++ src/pages/Aboutme.jsx
@@ -2,15 +2,14 @@ import styles from "../components.css/Aboutme.module.css";
 
 function Aboutme() {
   return (
-    <div className={styles.container}>
-      <article className={styles.aboutme__container}>
-        <img
-          className={styles.profilpic}
-          src="https://as2.ftcdn.net/v2/jpg/05/90/45/35/1000_F_590453560_ugMuPncnGYB6XnJqmC8xiPQx4eg3jmMD.jpg"
-          alt="Profilpic"
-        />
-
-        <p className={styles.paragraph}>
+    <article className={styles.article}>
+      <img
+        className={styles.profilpic}
+        src="https://as2.ftcdn.net/v2/jpg/05/90/45/35/1000_F_590453560_ugMuPncnGYB6XnJqmC8xiPQx4eg3jmMD.jpg"
+        alt="Profilpic"
+      />
+      <section className={styles.aboutMe__section}>
+        <p className={styles.p}>
           Hej, jag heter Johan och jag är 27 år gammal, och just nu är jag på en
           spännande resa mot att bli en front-end utvecklare. Min passion för
           teknik och design har lett mig till en värld där kreativitet möter
@@ -18,20 +17,22 @@ function Aboutme() {
           månader sedan påbörjade jag denna resa, och sedan dess har jag varit
           helt uppslukad av att lära mig allt som har med front-end utveckling
           att göra.
-          <p className={styles.paragraph}>
-            Min utbildning har inte bara gett mig tekniska färdigheter i HTML,
-            CSS, JavaScript och React, men också en djupare förståelse för hur
-            viktigt det är att skapa användarvänliga och tillgängliga
-            webbplatser. Jag älskar utmaningar och ser dem som möjligheter att
-            växa. Att ständigt utmana mig själv och lära mig nya saker är något
-            som driver mig. Jag tror fast på att bästa sättet att lära sig är
-            genom att göra, experimentera och ibland göra misstag. Varje kodrad
-            jag skriver och varje projekt jag tar mig an för mig ett steg
-            närmare min dröm om att bli en skicklig front-end utvecklare.
-          </p>
         </p>
-      </article>
-    </div>
+      </section>
+      <section className={styles.aboutMe__section}>
+        <p className={styles.p}>
+          Min utbildning har inte bara gett mig tekniska färdigheter i HTML,
+          CSS, JavaScript och React, men också en djupare förståelse för hur
+          viktigt det är att skapa användarvänliga och tillgängliga webbplatser.
+          Jag älskar utmaningar och ser dem som möjligheter att växa. Att
+          ständigt utmana mig själv och lära mig nya saker är något som driver
+          mig. Jag tror fast på att bästa sättet att lära sig är genom att göra,
+          experimentera och ibland göra misstag. Varje kodrad jag skriver och
+          varje projekt jag tar mig an för mig ett steg närmare min dröm om att
+          bli en skicklig front-end utvecklare.
+        </p>
+      </section>
+    </article>
   );
 }
 
