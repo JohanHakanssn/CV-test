97036a8bd06660d3d4bb103fa3b8756761ca0cf6
[Johan Håkansson] <[johan1hakansson@gmail.com]>
2024-02-01 09:28:09 +0100

    ...

--- src/components/CardSkillsProps.jsx
+++ src/components/CardSkillsProps.jsx
@@ -1,35 +1,31 @@
 import styles from "./CardSkills.module.css";
+import image1 from "../images/CSS-Logo.jpg";
+import image2 from "../images/html-logo.jng";
+import image3 from "../images/Javascript logo.png";
+import image4 from "../images/React-icon.svg.png";
 
 function Skills(props) {
   return (
-    <div className={styles.grid__container}>
-      <article className={styles.grid}>
-        <span
-          className={styles.card}
-          style={{ backgroundColor: "rgba(255, 0, 0, 0.9)" }}
-        >
-          {props.skill1}
-        </span>
-        <span
-          className={styles.card}
-          style={{ backgroundColor: "rgba(38, 76, 228, 0.8)" }}
-        >
-          {props.skill2}
-        </span>
-        <span
-          className={styles.card}
-          style={{ backgroundColor: "rgba(247, 222, 30, 0.95)" }}
-        >
-          {props.skill3}
-        </span>
-        <span
-          className={styles.card}
-          style={{ backgroundColor: "rgba(97, 220, 251, 0.89)" }}
-        >
-          {props.skill4}
-        </span>
-      </article>
-    </div>
+    <section className={styles.card__container}>
+      <div className={styles.card}>
+        {props.skill1}
+        <img src={image2} alt="HTML.logo" />
+      </div>
+
+      <div className={styles.card}>
+        {props.skill2}
+        <img src={image1} alt="CSS.logo" />
+      </div>
+
+      <div className={styles.card}>
+        {props.skill3}
+        <img src={image3} alt="Javascript.logo" />
+      </div>
+      <div className={styles.card}>
+        {props.skill4}
+        <img src={image4} alt="REACT.logo" />
+      </div>
+    </section>
   );
 }
 export default Skills;
