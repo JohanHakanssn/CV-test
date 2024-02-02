b94460a80a39366fb72a75131bbd27c08c4d26c7
[Johan Håkansson] <[johan1hakansson@gmail.com]>
2024-02-02 09:18:37 +0100

    animation

--- src/components/CardSkillsProps.jsx
+++ src/components/CardSkillsProps.jsx
@@ -3,20 +3,12 @@ import styles from "../components.css/CardSkills.module.css";
 function Skills(props) {
   return (
     <section className={styles.card__container}>
-      <div className={styles.card} style={{ backgroundColor: "red" }}>
-        {props.skill1}
-      </div>
+      <div className={styles.card}>{props.skill1}</div>
 
-      <div className={styles.card} style={{ backgroundColor: "blue" }}>
-        {props.skill2}
-      </div>
+      <div className={styles.card}>{props.skill2}</div>
 
-      <div className={styles.card} style={{ backgroundColor: "orange" }}>
-        {props.skill3}
-      </div>
-      <div className={styles.card} style={{ backgroundColor: "lightblue" }}>
-        {props.skill4}
-      </div>
+      <div className={styles.card}>{props.skill3}</div>
+      <div className={styles.card}>{props.skill4}</div>
     </section>
   );
 }
